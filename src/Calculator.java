670e21572e1a94b9731fc4471030c6d266a203ab
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-03-23 23:33:58 -0600

    [Created] Calculator class and Jar File with Ant

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a+b;
+
+    }
+    public int subtract(int a, int b){
+        return a-b;
+
+    }
+    public int multiply(int a, int b){
+        return a*b;
+
+    }
+    public int divide(int a, int b){
+        return a/b;
+
+    }
+
+    /*public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int res = calc.sum(10,5);
+        System.out.println(res);
+    }*/
+
+}
